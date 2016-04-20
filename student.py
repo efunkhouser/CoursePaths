43186496b7583a9d5065d68bf37f96318542e492
Emma Price <emma.price@students.olin.edu>
2016-04-20 19:00:04 -0400

    Added files via upload

--- student.py
+++ student.py
@@ -4,26 +4,20 @@ import csv
 import copy
 import numpy
 import numpy.random as nprnd
-from dictionaries import *
-# from course_dict import Course
+from dicts_classes import *
 
-# f = 'CourseTypes_Majors.csv'
-# df = pd.read_csv(f)
-# '''Create GENERAL, the global list of general-requirement Course objects'''
-# GENERAL_strings = ['ModSim', 'ModBio', 'Lin1', 'Lin2', 'ISIM', 'POE', 'E!Foundation', 'DesNat', 'UOCD', 'AHSE Foundation', 'AHSE 2', 'AHSE 3', 'AHSE 4', 'Chem/MatSci', 'ProbStat', ['E&M', 'Mechanics'], 'Design Depth', 'SCOPE/ADE1', 'SCOPE/ADE2']
-# GENERAL = [course_objects[course_name] for course_name in GENERAL_strings]
-
-
-'''Now define Student class'''
 class Student(object):
     """determines the number of credits left in each course coloring (eng, ahse, math, science)
     also determines which major and general requirements are left to take"""
-    def __init__(self, semesters_left=None, major_name = 'MechE' , courses_taken = ['ModSim', 'ISIM', 'Design Nature', 'AHSE Foundation'], courses_left=[]):
+    def __init__(self, semesters_left=None, major_name = 'MechE' , courses_taken = ['ModSim', 'ISIM', 'Design Nature', 'AHSE Foundation'], courses_left=None):
         self.Major = major_objects[major_name]
-        self.courses_taken = [course_objects[course_name] for course_name in courses_taken]
+        self.courses_taken = [course_objects[course_name] for course_name in courses_taken] #list of Course objects
         self.courses_left = courses_left #possible bug: if courses_left is not None it'll get overwritten when major_requirements_left is run
         #Addtl note on self.courses_left: must be a list of Course objects
         self.semesters_left = 0 if semesters_left==None else semesters_left
+        
+    def __str__(self):
+    	return "Major: {}.\nSemesters remaining: {}.\nCourses taken: {}.\nCourses needed: {}.".format(self.Major.name, self.semesters_left, [course.name for course in self.courses_taken], [course.name for course in self.courses_left])
         
     def eng_counter(self):
         eng_credit_required = 46
@@ -54,76 +48,103 @@ class Student(object):
         
     def major_requirements_left(self):
         if self.courses_left==None:
-            self.courses_left = self.Major.major_requirements
+        	self.courses_left = []
+        	self.courses_left = self.Major.major_requirements
         else:
-            pass
-        ###TODO: resolve: What are we looking for when they pass in "courses_left"? Non-required courses that they want to take? Required and non-required? Nothing at all?""
+            for course in self.Major.major_requirements:
+            	if course in self.courses_left:
+            		pass
+            	else:
+            		self.courses_left.append(course)
+            		
         for course in self.courses_left:
-            if course in self.courses_taken and self.Major.major_requirements:
-                self.courses_left -= self.courses_taken
+            if course in self.courses_taken:
+            	self.courses_left.remove(course)
                 
     def given_coursedict_make_schedule(self):
-	'''This is a method of the Student class.
-		It needs two attributes belonging to the Student object:
-			courses_left, which is a list of Course objects that this method must arrange into a schedule, and
-			semesters_left, which is the number of semesters in which to fit these Courses.
-		It returns a dictionary whose keys are the semesters remaining and values are classes in those semesters.'''
-	course_list = copy.deepcopy(self.courses_left) #list of Course objects
+		'''This is a method of the Student class.
+			It needs two attributes belonging to the Student object:
+				courses_left, which is a list of Course objects that this method must arrange into a schedule, and
+				semesters_left, which is the number of semesters in which to fit these Courses.
+			It returns a dictionary whose keys are the semesters remaining and values are classes in those semesters.'''
+			
+		self.major_requirements_left() #Populates self.courses_left
+		course_list = copy.deepcopy(self.courses_left) #list of Course objects
+		
+		semester_list = []
+		for number in range((8 - self.semesters_left), 8):
+			semester_list.append(Semester(number)) #This creates however many Semesters the Student needs
 	
-	semester_list = []
-	for number in range(self.semesters_left):
-		semester_list.append(Semester(number)) #This creates however many Semesters the Student needs
+		### Place classes that must be taken in a specific semester
+		for course in course_list:
+			for semester in semester_list:
+				if course.required_sem == semester.number: #If we already know when the class must be taken,
+					semester.base_courses.append(course) #	    #Move it to the appropriate semester
+					course_list.remove(course)#					#And remove it from the course_list for later
+		
+		### Now we begin trying random orders of classes and comparing them to find out which has the most even spread of difficulty
+		random_semester_assigns = []
+		min_difficulty = 20.0 * self.semesters_left #Arbitrary high 'minimum difficulty' threshold
+		best_distribution_of_classes = None
+		
+		for i in range(10000):
+			'''Try 1000 random arrangements of courses in semesters and store arrangements that spread difficulty evenly'''
+			random_semester_assigns[:] = nprnd.randint((8 - self.semesters_left), 8, size=len(course_list))
+			
+			for semester in semester_list:
+				semester.difficulty = 0
+				semester.courses[:] = []
+				
+				#Assign random semester numbers to what's left in course_list
+				sem_indices = numpy.where(numpy.array(random_semester_assigns) == semester.number)[0]
+				semester.courses = semester.base_courses + [course_list[i] for i in sem_indices]
+				
+				#Now calculate the difficulty of each semester
+				for course in semester.courses:
+					semester.difficulty += course.difficulty
+					
+			if all(semester.difficulty < min_difficulty for semester in semester_list):
+				min_difficulty = max(semester.difficulty for semester in semester_list)
+				best_distribution_of_classes = copy.copy(random_semester_assigns)
 
-	### Place classes that must be taken in a specific semester
-	for course in course_list:
+		course_plan_dict = {}
+		
 		for semester in semester_list:
-			if course.required_semester == semester.number: #If we already know when the class must be taken,
-				semester.base_courses.append(course) #	    #Move it to the appropriate semester
-				course_list.remove(course)#					#And remove it from the course_list for later
-
-	
-	### Now we begin trying random orders of classes and comparing them to find out which has the most even spread of difficulty
-	random_semester_assigns = []
-	min_difficulty = 20.0 * self.semesters_left #Arbitrary high 'minimum difficulty' threshold
-	best_distribution_of_classes = None
-	
-	for i in range(1000):
-		'''Try 1000 random arrangements of courses in semesters and store arrangements that spread difficulty evenly'''
-		random_semester_assigns[:] = nprnd.randint((8 - self.semesters_left), 8, size=len(course_list))
-		for semester in semester_list:
-			semester.difficulty = 0
+			semester.difficulty = 0 #Reset these values before reassigning them to the best distribution values
 			semester.courses[:] = []
 			
-			#Assign random semester numbers to what's left in course_list (1 or 2 in this case)
-			sem_indices = numpy.where(random_semester_assigns == semester.number)[0]
-		
-			semester.courses = semester.base_courses + [course_list[i] for i in sem_indices]
+			sem_indices = numpy.where(numpy.array(best_distribution_of_classes) == semester.number)[0]
+			semester.courses = semester.base_courses + [course_list[i] for i in sem_indices] #Required courses + whatever random arrangement worked best
 			
-			#Now calculate the difficulty of each semester
-			for course in semester.courses:
-				semester.difficulty += course.difficulty
-				
-		if all(semester.difficulty < min_difficulty for semester in semester_list):
-			min_difficulty = max(semester.difficulty for semester in semester_list)
-			best_distribution_of_classes = copy.copy(random_semester_assigns)
-	
-	#Now reassign with the chosen best courses for each semester
-	for semester in semester_list:
-		semester.difficulty = 0
-		semester.courses[:] = []
-		sem_indices = numpy.where(best_distribution_of_classes == semester.number)[0]
-		semester.courses = semester.base_courses + [course_list[i] for i in sem_indices]
-	
-		print 'Semester {):'.format(semester.number)
-		for course in semester.courses:
-			print course
-			semester.difficulty += course.difficulty
-		print 'Total semester difficulty: {}'.format(semester.difficulty)
-                
-                
+			course_plan_dict[semester.number] = semester.courses #Format: {semester number: [Course1, Course2, Course3], ...}
+			#Possible modification: Semester object as key rather than sem # int
+		return course_plan_dict
+
+
 '''Create a Student object for the user and prompt them for their major, courses taken, and semesters remaining'''
-stu_major = raw_input("What's your major? (E:C, BioE, DesignE, ECE, MechE, RoboE, MatSciE) \n>>")
-stu_courses_taken = raw_input("What courses have you taken? \n>>")
-stu_courses_taken_list = [course.strip() for course in stu_courses_taken.split(',')]
-stu_semesters_left = int(raw_input("How many semesters do you have left? \n>>"))
-stu = Student(stu_semesters_left, stu_major, stu_courses_taken_list)
+# stu_major = raw_input("What's your major? (E:C, BioE, DesignE, ECE, MechE, RoboE, MatSciE) \n>>")
+# stu_courses_taken = raw_input("What courses have you taken? \n>>")
+# stu_courses_taken_list = [course.strip() for course in stu_courses_taken.split(',')]
+# stu_semesters_left = int(raw_input("How many semesters do you have left? \n>>"))
+# stu = Student(stu_semesters_left, stu_major, stu_courses_taken_list)
+
+if __name__ == '__main__':
+	stu = Student(6, 'MechE', ['ISIM', 'DesNat', 'ModSim', 'AHSE Foundation'])
+	print stu.given_coursedict_make_schedule()
+
+### Code graveyard
+		
+			# print 'Semester {}:'.format(semester.number)
+			# for course in semester.courses:
+			# 	print course
+			# 	semester.difficulty += course.difficulty
+			# print 'Total semester difficulty: {}'.format(semester.difficulty)
+			
+# from course_dict import Course
+
+# f = 'CourseTypes_Majors.csv'
+# df = pd.read_csv(f)
+
+# '''Create GENERAL, the global list of general-requirement Course objects'''
+# GENERAL_strings = ['ModSim', 'ModBio', 'Lin1', 'Lin2', 'ISIM', 'POE', 'E! Foundation', 'DesNat', 'UOCD', 'AHSE Foundation', 'AHSE 2', 'AHSE 3', 'AHSE 4', 'Chem/MatSci', 'ProbStat', 'E&M', 'Mechanics', 'Design Depth', 'SCOPE/ADE1', 'SCOPE/ADE2']
+# GENERAL = [course_objects[course_name] for course_name in GENERAL_strings]
